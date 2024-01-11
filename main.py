4ae5605cc553e520924eed2969e7d3192c3f3cc8
Your Name <you@example.com>
2024-01-11 14:06:14 +0700

    now

--- main.py
+++ main.py
@@ -15,4 +15,20 @@ fiter.bright_image(0.7)
 
 fiter.imageshow()
 
+from samgeo.text_sam import LangSAM
+
+
+sam = LangSAM()
+
+text_prompt = "fram"
+
+sam.predict(fiter, text_prompt, box_threshold=0.2, text_threshold=0.2)
+
+sam.show_anns(
+    cmap='Accent_r',
+    box_color='red',
+    title=f'Automatic Segmentation of {text_prompt}',
+    blend=True,
+    # output= f'{output_path}{text_prompt}_detection_boxs.tif'
+)
 
