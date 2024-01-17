66ff608a4fbc8390a8b20d2c16abbd135e114926
BuKeMod <Xpotion2@gmail.com>
2024-01-17 12:03:31 +0700

    Update argumentParser.py

--- argumentParser.py
+++ argumentParser.py
@@ -4,7 +4,7 @@ def create_parser():
     parser = argparse.ArgumentParser(description='Your program description')
 
 
-    parser.add_argument('--image_path', type=int, default=12, help='image_path')
+    parser.add_argument('--image_path', type=str, default='images', help='image_path')
     parser.add_argument('--batch', type=bool, default=False, help='predict batch image')
     # Training configuration
     parser.add_argument('--model_type', type=str, default='vit_h', help='model_type segment') 
