0785d695e0dbcf7f106aa7ac83bc35ed0f083483
BuKeMod <Xpotion2@gmail.com>
2024-01-18 17:55:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def image_segment(image_path, output_path='segment_output', brightscale=1, batch
         # ให้ ImageFilter ปรับภาพแต่ละรูป
         for image_filter in image_filters:
 
-            image_filter.bright_image(brightscale)
+            if brightscale != 1:
+                image_filter.bright_image(brightscale)
             image = image_filter.get_image_temp(
                 output_path=output_path, quality=100)
 
@@ -24,13 +25,13 @@ def image_segment(image_path, output_path='segment_output', brightscale=1, batch
 
             filename = image_filter.get_filename()
             print(f"*{filename} segment success")
-     
 
     else:
         print("single image segment process")
 
         image_filter = imagefilter(image_path)
-        image_filter.bright_image(brightscale)
+        if brightscale != 1:
+            image_filter.bright_image(brightscale)
         image = image_filter.get_image_temp(
             output_path=output_path, quality=100)
 
@@ -53,13 +54,13 @@ def image_detection(image_path, output_path='detection_output', brightscale=1, t
         # ให้ ImageFilter ปรับภาพแต่ละรูป
         for image_filter in image_filters:
 
-            image_filter.bright_image(brightscale)
+            if brightscale != 1:
+                image_filter.bright_image(brightscale)
             image = image_filter.get_image_temp(
                 output_path=output_path, quality=100)
 
             detection(image, output_path, text_prompt,
                       box_threshold, text_threshold)
-          
 
             filename = image_filter.get_filename()
             print(f"*{filename} detection success")
@@ -67,13 +68,14 @@ def image_detection(image_path, output_path='detection_output', brightscale=1, t
         print("---single image detection process---")
 
         image_filter = imagefilter(image_path)
-        image_filter.bright_image(brightscale)
+        if brightscale != 1:
+            image_filter.bright_image(brightscale)
         image = image_filter.get_image_temp(
             output_path=output_path, quality=100)
 
         detection(image, output_path, text_prompt,
                   box_threshold, text_threshold)
-        
+
         filename = image_filter.get_filename()
         print(f"{filename} detection success")
 
@@ -84,7 +86,8 @@ if __name__ == '__main__':
     args = create_parser()
     model_type = args.model_type
     text_prompt = 'house'
-    image_segment(image_path=args.image_path,brightscale=1.2,batch=args.batch,model_type=model_type)
+    image_segment(image_path=args.image_path, brightscale=1.2,
+                  batch=args.batch, model_type=model_type)
     # image_detection(image_path=args.image_path, text_prompt=text_prompt,
     #                 brightscale=1.2, box_threshold=0.2, text_threshold=0.2)
 
